45cddf544bb2a74c4ca34d72832d3b8f5a9c83e6
sekikazuya <88357511+sekikazuya@users.noreply.github.com>
2021-10-05 16:21:35 +0900

    トップページ編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,22 +5,4 @@ class UsersController < ApplicationController
     @company = @user.company
   end
 
-  def edit
-    
-  end
-
-  def update
-    if current_user.update(user_params)
-      redirect_to user_path(current_user.id)
-    else
-      render :edit
-    end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :company, :occupation, :position)
-  end
-
 end
