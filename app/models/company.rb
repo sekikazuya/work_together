741533971c462676bb72c8782370cef9d5f4ee98
sekikazuya <88357511+sekikazuya@users.noreply.github.com>
2021-10-05 16:56:27 +0900

    アソシエーションの設定

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,14 @@
 class Company < ApplicationRecord
+  belongs_to :user
+  
+  with_options presence: true do
+    validates :name,
+              :name_kana,
+              :sector_id,
+              :industry_id,
+              :profile,
+              :prefecture_id,
+              :city,
+              :phone,
+  end
 end
