e5f13cc0c998b0cfbfbb8d8246bcfc930501af61
sekikazuya <88357511+sekikazuya@users.noreply.github.com>
2021-10-05 18:15:51 +0900

    new,createアクションとルーティングを設定

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,4 +1,13 @@
 class CompaniesController < ApplicationController
   def index
   end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
 
   root to: "companies#index"
   resources :users, only: [:show]
+  resources :companies, only: [:index, :new, :create]
 end
