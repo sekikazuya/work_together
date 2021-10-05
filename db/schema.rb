8d8327158dea1ee625b44ff3a95f469363e4343a
sekikazuya <88357511+sekikazuya@users.noreply.github.com>
2021-10-05 16:48:46 +0900

    companiesテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_30_084057) do
+ActiveRecord::Schema.define(version: 2021_10_05_073111) do
+
+  create_table "companies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "name_kana", null: false
+    t.integer "sector_id", null: false
+    t.integer "industry_id", null: false
+    t.text "profile", null: false
+    t.integer "prefecture_id", null: false
+    t.string "city", null: false
+    t.string "building_name"
+    t.string "phone", null: false
+    t.text "business_hours"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_companies_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -28,4 +45,5 @@ ActiveRecord::Schema.define(version: 2021_09_30_084057) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "companies", "users"
 end
