93254f7eb13c5583488d0eee0b29d8dfa5301299
sekikazuya <88357511+sekikazuya@users.noreply.github.com>
2021-10-13 18:07:22 +0900

    popperを記述

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("bootstrap/dist/js/bootstrap")
 require("jquery")
+require("popper")
 require("bootstrap")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
