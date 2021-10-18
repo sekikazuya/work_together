8a2725e28136f208eb00544d9a307fd95a451b97
sekikazuya <88357511+sekikazuya@users.noreply.github.com>
2021-10-18 11:16:22 +0900

    画像飲みでもメッセージを送信可能となるバリデーションの設定

--- app/models/iquiry.rb
+++ app/models/iquiry.rb
@@ -3,5 +3,9 @@ class Iquiry < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :message, presence: true
+  validates :message, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
 end
